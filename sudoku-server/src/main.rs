1683fedad929bd5ef42d71be461e96cd57b7d3b4
Zachary Stewart <zachary@zstewart.com>
2020-12-27 00:04:18 -0500

    implement sudoku server

---
+++ sudoku-server/src/main.rs
@@ -0,0 +1,77 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::Responder;
+use rocket_contrib::json::Json;
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+use sudoku_lib::{Board, Coord};
+
+/// Result of attempting to solve the sudoku puzzle.
+#[derive(Error, Debug, Responder)]
+enum SolveFailure {
+    #[response(status = /* Unprocessable Entity */ 422)]
+    #[error("{0}")]
+    BadRequest(String),
+    #[response(status = /* Conflict */ 409)]
+    #[error("{0}")]
+    NoSolution(String),
+}
+
+#[derive(Serialize, Deserialize)]
+struct Solution {}
+
+#[post("/api/sudoku/solve", data = "<board>")]
+fn solve(board: Json<Vec<Vec<Option<u8>>>>) -> Result<Json<Vec<Vec<u8>>>, SolveFailure> {
+    let raw_board = board.into_inner();
+    if raw_board.len() != 9 {
+        return Err(SolveFailure::BadRequest(format!(
+            "Expected 9 rows, got {}",
+            raw_board.len()
+        )));
+    }
+    if let Some((i, wrong)) = raw_board.iter().enumerate().find(|(_, row)| row.len() != 9) {
+        return Err(SolveFailure::BadRequest(format!(
+            "Expected 9 colums, got {} on row {}",
+            wrong.len(),
+            i
+        )));
+    }
+    let mut board = Board::new();
+    for (y, row) in raw_board.iter().enumerate() {
+        for (x, &col) in row.iter().enumerate() {
+            if let Some(val) = col {
+                if !(1..=9).contains(&val) {
+                    return Err(SolveFailure::BadRequest(format!(
+                        "Values must be in range [1, 9], got {} on row {} column {}",
+                        val, y, x
+                    )));
+                }
+                board.specify(Coord::new(y, x), val);
+            }
+        }
+    }
+    if let Some(solution) = board.solve() {
+        let mut res = Vec::with_capacity(Board::HEIGHT);
+        for r in 0..Board::HEIGHT {
+            let mut row = Vec::with_capacity(Board::WIDTH);
+            for c in 0..Board::WIDTH {
+                row.push(
+                    solution
+                        .get(Coord::new(r, c))
+                        .expect("Solution was expected to have all cells with single known values"),
+                );
+            }
+            res.push(row);
+        }
+        Ok(Json(res))
+    } else {
+        Err(SolveFailure::NoSolution("No solution found".to_string()))
+    }
+}
+
+#[launch]
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![solve])
+}
